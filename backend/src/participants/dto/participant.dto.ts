26380e59345ef0cb453dba55b9d444e5a359dcbb
abdegenius <johnnieabijah2000@gmail.com>
2025-09-21 09:37:53 +0100

    feat: added participants module

---
+++ backend/src/participants/dto/participant.dto.ts
@@ -0,0 +1,18 @@
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
+export class JoinParticipantDto {
+  @IsNotEmpty()
+  @IsString()
+  roomId: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(30)
+  pseudonym: string;
+}
+
+export class LeaveParticipantDto {
+  @IsNotEmpty()
+  @IsString()
+  roomId: string;
+}
