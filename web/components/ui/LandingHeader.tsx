7f40fa41af978ff98348c45e33fc2d853ef19db8
Asher <141028690+No-bodyq@users.noreply.github.com>
2025-10-28 05:44:23 +0100

    small changes

--- web/components/ui/LandingHeader.tsx
+++ web/components/ui/LandingHeader.tsx
@@ -1,32 +1,52 @@
-'use client'
+"use client";
 
-import {
-  LogOut,
-  X,
-  Search, ArrowRight,
-} from 'lucide-react';
-import { Dialog, DialogPanel } from '@headlessui/react'
+import { LogOut, X, Search, ArrowRight } from "lucide-react";
+import { Dialog, DialogPanel } from "@headlessui/react";
 import { usePathname } from "next/navigation";
 import MobileProgresBar from "@/components/MobileProgresBar";
-
 
 import Image from "next/image";
 
 import gossipLogo from "@/images/logos/gossip.svg";
-import logoMobile from "@/images/logos/logo.svg"
+import logoMobile from "@/images/logos/logo.svg";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const navigation = [
-  { name: 'home', href: '/'},
-  { name: 'about', href: '#'},
-  { name: 'partners', href: '#'},
-]
+  { name: "home", href: "/" },
+  { name: "about", href: "#" },
+  { name: "partners", href: "#" },
+];
+
+const checkAuth = () => {
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("authToken");
+    return !!token;
+  }
+
+  return false;
+};
 
 export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const pathname = usePathname();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    setIsAuthenticated(checkAuth());
+  }, []);
+
+  const handleGetStarted = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (isAuthenticated) {
+      router.push("/dashboard");
+    } else {
+      router.push("/auth");
+    }
+  };
 
   return (
     <>
@@ -47,7 +67,9 @@ export default function Header() {
                   <Link
                     href={item.href}
                     className={`text-dark-white ${
-                      item.href === pathname ? 'text-light-teal' : 'hover:text-light-grey'
+                      item.href === pathname
+                        ? "text-light-teal"
+                        : "hover:text-light-grey"
                     }`}
                   >
                     <span className="text-center">{item.name}</span>
@@ -58,7 +80,7 @@ export default function Header() {
           </div>
 
           <div className="flex md:hidden">
-            <MobileProgresBar/>
+            <MobileProgresBar />
           </div>
 
           <Image
@@ -77,9 +99,11 @@ export default function Header() {
             {/*  />*/}
             {/*</button>*/}
             <button
-              className=" text-white flex shadow-[inset_0_0_12px_1px_#2F2F2F]  items-center space-x-2 px-6 py-4 rounded-full hover:opacity-80 cursor-pointer transition-colors">
-              <span>Connect Wallet</span>
-              <ArrowRight/>
+              onClick={handleGetStarted}
+              className=" text-white flex shadow-[inset_0_0_12px_1px_#2F2F2F]  items-center space-x-2 px-6 py-4 rounded-full hover:opacity-80 cursor-pointer transition-colors"
+            >
+              <span>Get Started</span>
+              <ArrowRight />
             </button>
           </div>
 
@@ -89,7 +113,7 @@ export default function Header() {
             className="lg:hidden p-2 rounded-lg text-gray-400 hover:text-white hover:bg-[#1A2221]"
             onClick={() => setMobileMenuOpen(true)}
           >
-            <Search className="w-5 h-5 text-gray-400"/>
+            <Search className="w-5 h-5 text-gray-400" />
           </button>
         </div>
 
@@ -112,10 +136,14 @@ export default function Header() {
         {/*  </div>*/}
         {/*</div>*/}
 
-        <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-          <div className="fixed inset-0 z-50"/>
-          <DialogPanel
-            className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-[#0a0f1a] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <Dialog
+          as="div"
+          className="lg:hidden"
+          open={mobileMenuOpen}
+          onClose={setMobileMenuOpen}
+        >
+          <div className="fixed inset-0 z-50" />
+          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-[#0a0f1a] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <Image
                 src={gossipLogo}
@@ -128,7 +156,7 @@ export default function Header() {
                 onClick={() => setMobileMenuOpen(false)}
               >
                 <span className="sr-only">Close menu</span>
-                <X className="h-6 w-6" aria-hidden="true"/>
+                <X className="h-6 w-6" aria-hidden="true" />
               </button>
             </div>
             <div className="mt-6 flow-root">
@@ -139,23 +167,24 @@ export default function Header() {
                       key={item.name}
                       className={`flex w-full items-center gap-4 rounded-lg px-4 py-3 ${
                         pathname === item.href
-                          ? 'bg-[#0F5951] text-[#14F1D9]'
-                          : 'text-[#A3A9A6] hover:bg-[#1A2221] hover:text-white'
+                          ? "bg-[#0F5951] text-[#14F1D9]"
+                          : "text-[#A3A9A6] hover:bg-[#1A2221] hover:text-white"
                       }`}
                       onClick={() => setMobileMenuOpen(false)}
                     >
-
-                      <span className="text-base font-semibold capitalize">{item.name}</span>
+                      <span className="text-base font-semibold capitalize">
+                        {item.name}
+                      </span>
                     </button>
                   ))}
                 </div>
                 <div className="py-6">
                   <div className="flex items-center justify-between rounded-lg bg-[#1A2221] px-4 py-3">
-                    <span className="text-sm text-gray-400">0x1234Hksko...5678</span>
+                    <span className="text-sm text-gray-400">
+                      0x1234Hksko...5678
+                    </span>
                     <button className="p-2 btn-glass-effect rounded-full">
-                      <LogOut
-                        className="aspect-square w-auto h-14 p-3"
-                      />
+                      <LogOut className="aspect-square w-auto h-14 p-3" />
                     </button>
                   </div>
                 </div>
@@ -165,12 +194,11 @@ export default function Header() {
         </Dialog>
       </header>
       <div className="lg:hidden flex items-center justify-end relative top-14 right-14 bg-black">
-        <button
-          className=" text-white flex shadow-[inset_0_0_12px_1px_#2F2F2F]  items-center space-x-2 px-6 py-4 rounded-full hover:opacity-80 cursor-pointer transition-colors">
+        <button className=" text-white flex shadow-[inset_0_0_12px_1px_#2F2F2F]  items-center space-x-2 px-6 py-4 rounded-full hover:opacity-80 cursor-pointer transition-colors">
           <span>Connect Wallet</span>
-          <ArrowRight/>
+          <ArrowRight />
         </button>
       </div>
     </>
-  )
+  );
 }
